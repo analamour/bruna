2492a6f544865121e68a3153eeffaf97121a50c1
analamour <ana.lamour@outlook.com>
2023-08-07 20:43:42 -0300

    avances

--- js/index.js
+++ js/index.js
@@ -1,12 +1,19 @@
-const url = '../TrabajoPracticoTaller1Grupo7/productos.json';
-const request = new XMLHttpRequest();
-request.open('GET', url); // setean el método, la url de api
-request.responseType = 'json'; //definen el tipo de dato que les devuelve
-request.send(); //envían la solicitud
-var productos = '';
+// Obtén las referencias a la tarjeta y al portador de la imagen.
+let card = document.getElementById('card');
+let imageHolder = document.getElementById('imageHolder');
 
-request.onload = function() {
-    console.log(request.response);
-    productos = request.response;
-    setTablaDestacados(); //funcion que trae productos destacados
-}
+// Cuando el mouse se coloca sobre la tarjeta, añade la imagen.
+card.addEventListener('mouseover', function() {
+    let image = document.createElement('img');
+    image.src = 'img/logocerra.jpg'; // reemplaza 'ruta_de_tu_imagen.jpg' con la ruta de tu imagen
+    image.style.width = '100%';
+    image.style.height = '100%';
+    imageHolder.appendChild(image);
+});
+
+// Cuando el mouse sale de la tarjeta, elimina la imagen.
+card.addEventListener('mouseout', function() {
+    while (imageHolder.firstChild) {
+        imageHolder.removeChild(imageHolder.firstChild);
+    }
+});
